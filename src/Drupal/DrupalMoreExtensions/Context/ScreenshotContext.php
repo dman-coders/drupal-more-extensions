7a8dadab7e74ffe79507a1fc59aad31e15737878
Dan (dman) Morrison <dan@coders.co.nz>
2015-11-28 14:26:15 +1300

    Adjusted the resized screen sizes to show more. Added a process to snapshot an element in context. paramaterised the screenshot path and timestamp toggle so it can be set from above

--- src/Drupal/DrupalMoreExtensions/Context/ScreenshotContext.php
+++ src/Drupal/DrupalMoreExtensions/Context/ScreenshotContext.php
@@ -3,9 +3,14 @@
 namespace Drupal\DrupalMoreExtensions\Context;
 
 use Behat\MinkExtension\Context\RawMinkContext;
+use Behat\MinkExtension\Context\MinkAwareContext;
 
 /**
- * Defines application features from the specific context.
+ * Defines application features (actions) for working with screenshots.
+ *
+ * As it extends 'Mink' (to get the screenshot-ability) it is not natively
+ * aware of 'Behat' so needs help to know about things like behat scenarios
+ * or params.
  */
 class ScreenshotContext extends RawMinkContext {
 
@@ -15,6 +20,16 @@ class ScreenshotContext extends RawMinkContext {
    * @var string
    */
   protected $path;
+
+  /**
+   * Whether to include timestamps when constructing filepaths.
+   *
+   * This can be used either to compare multiple test runs (true)
+   * Or if you just want to collect a number of screenshots (false).
+   *
+   * @var string
+   */
+  protected $timestamped;
 
   /**
    * When the suite was started.
@@ -30,16 +45,33 @@ class ScreenshotContext extends RawMinkContext {
    *
    * Set up params - pathname and timestamp for saving the screenshot files.
    *
+   * These are expected to be provided by the behat.yml file,
+   * or optionally defined via commandline parameters.
+   *
+   *    default:
+   *      suites:
+   *        default:
+   *          contexts:
+   *            - Drupal\DrupalMoreExtensions\Context\ScreenshotContext:
+   *                params:
+   *                  path: 'screenshots'
+   *                  timestamped: false
+   *
+   * TAKE CARE ON THE INDENTS in YAML here ^
+   *
    * @param $params
    */
   public function __construct($params = array()) {
-
     $params += array(
       'path' => 'screenshots',
+      'timestamped' => TRUE,
     );
 
     $this->path = rtrim($params['path'], '/') . '/';
+    $this->timestamped = (bool) $params['timestamped'];
     $this->started = new \DateTime();
+    # TODO - the start time is currently only per-scanario, to per test run.
+    # so it's not doing its job as a collective run-task grouper.
   }
 
 
@@ -52,6 +84,7 @@ class ScreenshotContext extends RawMinkContext {
    * @Given the viewport is wide/medium/narrow/mobile
    */
   public function theViewportIs($size) {
+    // Viewports sometimes higher than natural to illustrate scrolled content.
     $dimensions = array(
       'wide' => array(
         'width' => 1600,
@@ -59,15 +92,15 @@ class ScreenshotContext extends RawMinkContext {
       ),
       'medium' => array(
         'width' => 1024,
-        'height' => 800,
+        'height' => 768,
       ),
       'narrow' => array(
         'width' => 740,
-        'height' => 600,
+        'height' => 1024,
       ),
       'mobile' => array(
         'width' => 320,
-        'height' => 480,
+        'height' => 740,
       ),
     );
     if (isset($dimensions[$size])) {
@@ -76,6 +109,7 @@ class ScreenshotContext extends RawMinkContext {
         ->resizeWindow($dimensions[$size]['width'], $dimensions[$size]['height']);
     }
     else {
+      // eep, need a namespace on exçeption!?
       throw new Exception('Unknown named screensize');
     }
   }
@@ -83,6 +117,10 @@ class ScreenshotContext extends RawMinkContext {
 
   /**
    * Take a snapshot of the browser.
+   *
+   * This version does NOT save the screenshot anywhere useful (just /tmp)
+   * and throws the result at your screen.
+   * Use for runtime diagnostics, NOT automated tests.
    *
    * This works for phantomjs, Selenium and other real browsers
    * that support screenshots.
@@ -98,6 +136,13 @@ class ScreenshotContext extends RawMinkContext {
     $this->open_image_file($filepath);
   }
 
+  /**
+   * Helper function for diagnostics. Throw the file into a desktop viewer.
+   *
+   * This will need to be different per OS.
+   *
+   * @param $filepath
+   */
   public function open_image_file($filepath) {
     if (PHP_OS === "Darwin" && PHP_SAPI === "cli") {
       exec('open -a "Preview.app" ' . $filepath);
@@ -112,7 +157,6 @@ class ScreenshotContext extends RawMinkContext {
    * @Then /^show me the HTML page$/
    */
   public function showMeTheHtmlPageInTheBrowser() {
-
     $html_data = $this->getSession()->getDriver()->getContent();
     $filepath = '/tmp/behat_page.html';
     file_put_contents($filepath, $html_data);
@@ -122,7 +166,17 @@ class ScreenshotContext extends RawMinkContext {
     };
   }
 
-  /////////////////////////////////////////////////
+  /**
+   * @Then I take a screenshot and save as :arg1
+   */
+  public function iTakeAScreenshotAndSaveAs($filename) {
+    // Snapshot the visible screen.
+    $dst_filepath = $this->getScreenshotPath() . $this->getFilepath($filename);
+    $this->ensureDirectoryExists($dst_filepath);
+    file_put_contents($dst_filepath, $this->getSession()->getScreenshot());
+  }
+
+    /////////////////////////////////////////////////
   /**
    * Functionality copied from Zodyac PerceptualDiffExtension.
    *
@@ -135,17 +189,18 @@ class ScreenshotContext extends RawMinkContext {
 
   /**
    * @Then /^take a screenshot of "([^"]*)" and save "([^"]*)"$/
+   * @Then /^take a screenshot of "([^"]*)" and save as "([^"]*)"$/
+   * @Then I take a screenshot of :arg1 and save :arg2
    *
    * https://gist.github.com/amenk/11208415
    */
   public function takeAScreenshotOfAndSave($selector, $filename) {
 
     // Element must be visible on screen - scroll if needed.
-    // The runner 'focus() doesn't do this for us.
     // Scroll to align bottom by default (if needed) as align top usually
     // doesn't tell the right story.
-    $js = 'return jQuery("' . $selector . '")[0].scrollIntoView(false);';
-    $this->getSession()->evaluateScript($js);
+    $javascript = 'return jQuery("' . $selector . '")[0].scrollIntoView(false);';
+    $this->getSession()->evaluateScript($javascript);
 
     // Snapshot the visible screen.
     $screen_filepath = '/tmp/' . $filename;
@@ -153,18 +208,38 @@ class ScreenshotContext extends RawMinkContext {
 
     // Get element dimensions for cropping.
     // This calculation requires jquery to be available on the page already.
-    $js = 'return jQuery("' . $selector . '")[0].getBoundingClientRect();';
-    $pos = $this->getSession()->evaluateScript($js);
+    $javascript = 'return jQuery("' . $selector . '")[0].getBoundingClientRect();';
+    $pos = $this->getSession()->evaluateScript($javascript);
 
     $dst_filepath = $this->getScreenshotPath() . $this->getFilepath($filename);
     $this->crop_and_save($screen_filepath, $pos, $dst_filepath);
     echo "Saved element screenshot as $dst_filepath";
     $this->open_image_file($dst_filepath);
 
-    /////////////////////////////
+    // Optionally
     // Create a context thumbnail.
+    $context_filepath =  $this->getScreenshotPath() . $this->getFilepath($filename . '-context');
+    $this->generateContextualizedScreenshotOfElement($screen_filepath, $dst_filepath, $context_filepath, $pos);
+    $this->open_image_file($context_filepath);
+  }
+
+  /**
+   * Create a context thumbnail of the selected element inside the whole page.
+   *
+   * Helper function for takeAScreenshotOfAndSave().
+   *
+   * @param string $screen_filepath
+   *   Full page image file path.
+   * @param string $element_filepath
+   *   Already generated element image file path.
+   * @param string $context_filepath
+   *   Destination path to save the highlighted compostie into.
+   * @param array $pos
+   *   Location and dimensions of the element.
+   */
+  private function generateContextualizedScreenshotOfElement($screen_filepath, $element_filepath, $context_filepath, $pos) {
     $src_image = imagecreatefrompng($screen_filepath);
-    $component_img = imagecreatefrompng($dst_filepath);
+    $component_img = imagecreatefrompng($element_filepath);
 
     // Blur the full screenshot.
     imagefilter($src_image, IMG_FILTER_SELECTIVE_BLUR);
@@ -179,20 +254,15 @@ class ScreenshotContext extends RawMinkContext {
     // Paste the (unblurred) cropped image back over top where it was.
     imagecopy($src_image, $component_img, $pos['left'], $pos['top'], 0, 0, $pos['width'], $pos['height']);
 
-    // Shrink it
-    $percent = 0.2;
+    // Shrink it all
+    $percent = 0.25;
     $width = imagesx($src_image);
     $height = imagesy($src_image);
     $newwidth = $width * $percent;
     $newheight = $height * $percent;
     $thumb_image = imagecreatetruecolor($newwidth, $newheight);
-    imagecopyresized($thumb_image, $src_image, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
-
-    $context_filepath =  $dst_filepath = $this->getScreenshotPath() . $this->getFilepath($filename . '-context');
+    imagecopyresampled($thumb_image, $src_image, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
     imagepng($thumb_image, $context_filepath);
-    #imagepng($src_image, $context_filepath);
-    $this->open_image_file($context_filepath);
-
   }
 
   /**
@@ -223,7 +293,12 @@ class ScreenshotContext extends RawMinkContext {
    * @return string
    */
   public function getScreenshotPath() {
-    return $this->path . $this->started->format('YmdHis') . '/';
+    if ($this->timestamped) {
+      return $this->path . $this->started->format('YmdHis') . '/';
+    }
+    else {
+      return $this->path;
+    }
   }
 
   /**
@@ -238,12 +313,6 @@ class ScreenshotContext extends RawMinkContext {
     // feature being tested. But I can't TELL which task or step is
     // currently running. It's difficult to get that from the context.
     return $this->formatString($filename) . '.png';
-
-    return sprintf('%s/%s/%s.png',
-      $this->formatString($this->currentScenario->getFeature()->getTitle()),
-      $this->formatString($this->currentScenario->getTitle()),
-      $filename
-    );
   }
 
   /**
