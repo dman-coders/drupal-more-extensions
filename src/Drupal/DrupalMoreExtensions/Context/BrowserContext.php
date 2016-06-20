ffa5b5702885108317603cddcbcd900aa57bfcee
Dan (dman) Morrison <dan@coders.co.nz>
2016-06-21 04:52:06 +1200

    Introduced file download and response status checking - even if using selenium

--- src/Drupal/DrupalMoreExtensions/Context/BrowserContext.php
+++ src/Drupal/DrupalMoreExtensions/Context/BrowserContext.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\DrupalMoreExtensions\Context;
 use Behat\MinkExtension\Context\RawMinkContext;
+use GuzzleHttp\Cookie\CookieJar;
+use GuzzleHttp\Cookie\SetCookie;
 
 /**
  * Provides additional Browser session management utilities.
@@ -75,5 +77,99 @@ class BrowserContext extends RawMinkContext {
     $safe_value = addcslashes($value, "'");
     // TODO?
  }
+
+
+  /**
+   * @When /^I try to download "([^"]*)"$/
+   *
+   * The Selenium or Webdriver sessions do not allow us access to the
+   * HTTP Response code if we just 'get' a page. Also, getting a PDF
+   * Stalls the browser interaction and we get stuck.
+   * Instead use iTryToDownload() to retrieve a file and get the headers.
+   *
+   * https://www.jverdeyen.be/php/behat-file-downloads/
+   *
+   * Tested with
+   * phantomjs 2.1.1,
+   * Selenium 2.41.0 + Chrome,
+   * guzzlehttp 6.2.0,
+   * Goutte 3.1.2.
+   *
+   * @param string $url
+   */
+  public function iTryToDownload($url) {
+    // Need to make an out-of-band request to the URL in question
+    // directly in order to retrieve its headers.
+    // Need to sustain cookies, because authentication is important.
+    $driver = $this
+      ->getSession()
+      ->getDriver();
+    if (! method_exists($driver, 'getWebDriverSession')) {
+      // We are just running goutte or something similar.
+      // GET the URL normally and Check the response as usual.
+      $driver->visit($url);
+      $this->headers = $driver->getResponseHeaders();
+      $this->statusCode = $driver->getStatusCode();
+      return;
+    }
+
+    $cookies = $driver
+      ->getWebDriverSession()
+      ->getAllCookies();
+
+    // Transfer the browser session cookies into Guzzle ones.
+    $jar = new CookieJar();
+    foreach ($cookies as $cookie_data) {
+      $cookie = new SetCookie($cookie_data);
+      // Actually, the keys are expected to be capitalized here....
+      $cookie->setName($cookie_data['name']);
+      $cookie->setValue($cookie_data['value']);
+      $cookie->setDomain($cookie_data['domain']);
+      $jar->setCookie($cookie);
+    }
+
+    $client = new \GuzzleHttp\Client([
+      'base_uri' => $this->getSession()->getCurrentUrl(),
+      'cookies' => $jar,
+    ]);
+
+    // It's OK if Guzzle throws errors. That's what we are expecting.
+    try {
+      $response = $client->get($url);
+      $this->headers = $response->getHeaders();
+      $this->statusCode = $response->getStatusCode();
+    } catch (\GuzzleHttp\Exception\ClientException $e) {
+      print $e->getMessage();
+    }
+
+
+    // Would be good if we could get the $driver->setStatusCode()
+    // so as to let other checks function normally, but that's off-limits.
+  }
+
+  /**
+   * @Then /^I should see response status code "?(?P<code>\d+)"?$/
+   *
+   * https://www.jverdeyen.be/php/behat-file-downloads/
+   */
+  public function iShouldSeeResponseStatusCode($statusCode) {
+    $responseStatusCode = $this->statusCode;
+
+    if (!$responseStatusCode == intval($statusCode)) {
+      throw new \Exception(sprintf("Did not see response status code %s, but %s.", $statusCode, $responseStatusCode));
+    }
+  }
+
+  /**
+   * @Then /^I should see in the header "([^"]*)":"([^"]*)"$/
+   *
+   * https://www.jverdeyen.be/php/behat-file-downloads/
+   */
+  public function iShouldSeeInTheHeader($header, $value) {
+    $headers = $this->headers();
+    if ($headers->get($header) != $value) {
+      throw new \Exception(sprintf("Did not see %s with value %s.", $header, $value));
+    }
+  }
 
 }
