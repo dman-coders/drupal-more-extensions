aab6aaf3da6380bcb03204dc5075ba6f45deb104
Dan Morrison <dan@coders.co.nz>
2018-02-16 02:14:14 +0700

    Add some clauses - goToNamedContent, ContentHasFieldValue, ContentHasFlagSet for direct manipulation of node values

--- src/Drupal/DrupalMoreExtensions/Context/DrupalContext.php
+++ src/Drupal/DrupalMoreExtensions/Context/DrupalContext.php
@@ -12,13 +12,29 @@ use Drupal\DrupalExtension\Context\RawDrupalContext;
 class DrupalContext extends RawDrupalContext {
 
 
+
+
+  /**
+   * Goes to a page based on content type and title
+   *
+   *
+   * @When I go to the page for the :type with the title :title
+   *
+   * @throws \Exception if named content not found
+   */
+  public function goToNamedContent($type, $title) {
+    $node = $this->load_node_by_type_and_title($type, $title);
+    $this->getSession()->visit($this->locatePath('/node/' . $node->nid));
+  }
+
+
   /**
    * Checks or creates content of the given type.
    *
    * @Given a/an :type (content )with the title :title exists
    */
   public function assumeNode($type, $title) {
-    $node = $this->load_node_by_type_and_title($type, $title);
+    $node = $this->load_node_by_type_and_title($type, $title, FALSE);
     if (! $node) {
       $node = $this->createNode($type, $title);
     }
@@ -31,16 +47,35 @@ class DrupalContext extends RawDrupalContext {
   /**
    * Sets a field on a content item. Item is presumed to already exist.
    *
-   * And the "team" titled "Behat Beetles" has a "event" field value of "Behat Trailwalker"
+   * And the "team" with the title "Behat Beetles" has a "event" field value of "Behat Trailwalker"
    *
-   * @Given the :type titled :title has a(n) :field_name field value of :field_value
+   * @Given the :type with the title :title has a(n) :field_name field value of :field_value
    */
   public function ContentHasFieldValue($type, $title, $field_name, $field_value) {
     // @throws not found
     $node = $this->load_node_by_type_and_title ($type, $title);
     $wrapper = entity_metadata_wrapper('node', $node);
-    $node->$field_name = array('value' => $field_value);
-    node_save($node);
+    // Only trigger save if needed.
+    if ($field_value != $node->$field_name) {
+      $node->$field_name = $field_value;
+      node_save($node);
+    }
+  }
+
+  /**
+   * Sets a flag on a content item. Item is presumed to already exist.
+   *
+   * And the "team" with the title "Behat Beetles" has the "promoted" flag "on"
+   *
+   * @Given the :type with the title :title has the :field_name flag (set )$field_value
+   */
+  public function ContentHasFlagSet($type, $title, $field_name, $field_value) {
+    $string_values = array(
+      'off' => 0,
+      'on' => 1,
+    );
+    if (isset($string_values[$field_value])) $field_value = $string_values[$field_value];
+    return $this->ContentHasFieldValue($type, $title, $field_name, $field_value);
   }
 
 
@@ -50,8 +85,10 @@ class DrupalContext extends RawDrupalContext {
    * @param $type
    * @param $title
    * @return bool|mixed
+   *
+   * @throws \Exception if named content not found
    */
-  protected function load_node_by_type_and_title($type, $title) {
+  protected function load_node_by_type_and_title($type, $title, $complain = TRUE) {
     $nid = db_select ('node', 'n')
       ->fields ('n', array ('nid'))
       ->condition ('n.title', $title)
@@ -59,7 +96,7 @@ class DrupalContext extends RawDrupalContext {
       ->orderBy ('nid', 'desc')
       ->execute ()
       ->fetchField ();
-    if (!$nid)
+    if (!$nid && $complain)
       throw new Exception("'No $type with this title : $title");
     $node = node_load($nid);
     return $node;
